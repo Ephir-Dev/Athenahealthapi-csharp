9258d7bb7f12c4a5843b7f177cf4dffa5bd53c40
Andrii Kurdiumov <kant2002@gmail.com>
2019-04-30 16:07:26 +0300

    Add appointment status constants

---
+++ Client/AppointmentStatusConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace AndriiKurdiumov.AuthenaHealth.Client
+{
+    public static class AppointmentStatusConstants
+    {
+        public static readonly string Cancelled = "x";
+        public static readonly string Future = "f";
+        public static readonly string Open = "o";
+        public static readonly string CheckedIn = "2";
+        public static readonly string CheckedOut = "3";
+        public static readonly string ChargeEntered = "4";
+    }
+}
